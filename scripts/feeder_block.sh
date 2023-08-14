9ee16429a501185ff96f832472c6e80cf1870091
clarsen-007 <52528227+clarsen-007@users.noreply.github.com>
2023-08-14 13:42:45 +0200

    Update feeder_block.sh

--- scripts/feeder_block.sh
+++ scripts/feeder_block.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+# Version 01.03.01.00
+
+## Feeder Block 1
+## Feeder Block Stamparm
+
+SETNAME1="feeder_block_stamparm"
+if [ -x `which curl` -a -x `which ipset` ]; then
+   feeder_block_stamparm_ips=$( curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null \
+                      | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1 )
+   logger -t "feeder_block_stamparm_ip_block" "Adding IPs to be blocked."
+   ipset flush $SETNAME1
+   sleep 5
+   ipset list $SETNAME1 &>/dev/null
+   ipset create $SETNAME1 iphash
+   iptables -I INPUT 1 -m set --match-set $SETNAME1 src -j DROP
+   iptables -A FORWARD -m set --match-set $SETNAME1 src -j DROP
+      for i in $feeder_block_stamparm_ips
+           do ipset add $SETNAME1 $i
+      done
+   logger -t "feeder_block_stamparm_ip_block" "$( ipset list $SETNAME1 | wc -l )"
+fi
