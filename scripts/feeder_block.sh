ece41b711de03c619021cacff238de9c476d7ecf
clarsen-007 <52528227+clarsen-007@users.noreply.github.com>
2023-08-21 13:37:10 +0200

    Update feeder_block.sh

--- scripts/feeder_block.sh
+++ scripts/feeder_block.sh
@@ -21,3 +21,22 @@ if [ -x `which curl` -a -x `which ipset` ]; then
       done
    logger -t "feeder_block_stamparm_ip_block" "$( ipset list $SETNAME1 | wc -l )"
 fi
+
+## Feeder Block 2
+## Feeder Block emergingthreats_compromised_ips
+
+SETNAME2="emergingthreats_compromised_ips"
+if [ -x `which curl` -a -x `which ipset` ]; then
+   feeder_block_emergingthreats_compromised_ips=$( curl --compressed https://rules.emergingthreats.net/blockrules/compromised-ips.txt 2>/dev/null )
+   logger -t "feeder_block_emergingthreats_compromised_ips_ip_block" "Adding IPs to be blocked."
+   ipset flush $SETNAME2
+   sleep 5
+   ipset list $SETNAME2 &>/dev/null
+   ipset create $SETNAME2 iphash
+   iptables -I INPUT 1 -m set --match-set $SETNAME2 src -j DROP
+   iptables -A FORWARD -m set --match-set $SETNAME2 src -j DROP
+      for i in $feeder_block_emergingthreats_compromised_ips
+           do ipset add $SETNAME2 $i
+      done
+   logger -t "feeder_block_emergingthreats_compromised_ips_ip_block" "$( ipset list $SETNAME2 | wc -l )"
+fi
