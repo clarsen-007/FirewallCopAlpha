7d17f5f6eeed4046f265c30856eb895670d2dc4b
clarsen-007 <52528227+clarsen-007@users.noreply.github.com>
2023-08-28 17:08:05 +0200

    Update feeder_block.sh

--- scripts/feeder_block.sh
+++ scripts/feeder_block.sh
@@ -48,7 +48,7 @@ fi
 ## Feeder Block blocklist.de - added in 01.05.00.00
 
 SETNAME3="blocklist_de_ips"
-if [ -x 'which curl' -a -x 'which ipset' ]; then
+if [ -x `which curl` -a -x `which ipset` ]; then
    feeder_block_blocklist_de_ips=$( curl --compressed https://lists.blocklist.de/lists/all.txt 2>/dev/null | grep -v ":" )
    logger -t "feeder_block_blocklist_de_ips_ip_block" "Adding IPs to be blocked."
    ipset flush $SETNAME3
