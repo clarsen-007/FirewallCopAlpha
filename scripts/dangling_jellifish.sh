fd0ff10c00e66c5aa112f7fec6d979d88f025354
clarsen-007 <52528227+clarsen-007@users.noreply.github.com>
2023-09-28 13:57:57 +0200

    Update dangling_jellifish.sh

--- scripts/dangling_jellifish.sh
+++ scripts/dangling_jellifish.sh
@@ -17,32 +17,14 @@ date >> $FILE
 
 ## Start Feeders
 
-FEEDERFILE=$( curl --compress https://raw.githubusercontent.com/clarsen-007/FirewallCopAlpha/main/scripts/dangling_jellifish.conf
-while read -r line; do  
-  
-#Readind each line in sequence  
-echo $line  
-done <read_file.txt
+FEEDERFILE=$( curl --compressed https://raw.githubusercontent.com/clarsen-007/FirewallCopAlpha/main/scripts/dangling_jellifish.conf 2>/dev/null \
+                       grep -v '#' | grep 'http' )
+
+FEEDLINELOADER () {
+  echo -e "$( $FEEDERFILE | cut -d'@' -f2 )_feeder_block" "Adding IPs to be blocked." | tee -a $FILE
+}
 
 
-
-
-
-if [ -x `which curl` -a -x `which ipset` ]; then
-   feeder_block_emergingthreats_compromised_ips=$Feeders
-   logger -t "feeder_block_emergingthreats_compromised_ips_ip_block" "Adding IPs to be blocked."
-   ipset flush $SETNAME2
-   sleep 3
-   ipset create $SETNAME2 iphash 2>/dev/null
-   sleep 2
-   iptables -I INPUT 1 -m set --match-set $SETNAME2 src -j DROP
-   iptables -A FORWARD -m set --match-set $SETNAME2 src -j DROP
-      for i in $feeder_block_emergingthreats_compromised_ips
-           do ipset add $SETNAME2 $i
-      done
-   logger -t "feeder_block_emergingthreats_compromised_ips_ip_block" "$( ipset list $SETNAME2 | wc -l )"
-   echo -n "[$(date +"%d/%m/%Y %H:%M:%S")] " | tee -a $FILE
-   ipset list $SETNAME2 | head -7 | tee -a $FILE
-fi
-
-
+while read -r line
+  do FEEDLINELOADER
+done < read_file.txt
