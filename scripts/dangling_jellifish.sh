86e01e3baa153ce40d43d578d104942efec04735
clarsen-007 <52528227+clarsen-007@users.noreply.github.com>
2023-10-02 13:31:29 +0200

    Update dangling_jellifish.sh

--- scripts/dangling_jellifish.sh
+++ scripts/dangling_jellifish.sh
@@ -19,7 +19,7 @@ date >> $FILE
 
 ## Start Feeders
 
-wget -o $TEMPFOLDER https://raw.githubusercontent.com/clarsen-007/FirewallCopAlpha/main/scripts/dangling_jellifish.conf | grep -v '#' 2>/dev/null
+wget -P $TEMPFOLDER https://raw.githubusercontent.com/clarsen-007/FirewallCopAlpha/main/scripts/dangling_jellifish.conf | grep -v '#' 2>/dev/null
 
 # while read -r line
 #  do FEEDLINELOADER
