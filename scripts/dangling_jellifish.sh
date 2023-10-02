bb465474f11488cdbb6425fdd5d2052edfb0ad49
clarsen-007 <52528227+clarsen-007@users.noreply.github.com>
2023-10-02 14:15:00 +0200

    Update dangling_jellifish.sh

--- scripts/dangling_jellifish.sh
+++ scripts/dangling_jellifish.sh
@@ -26,24 +26,24 @@ sleep 1
 rm $TEMPFOLDER/dangling_jellifish.conf
 
 FEEDER () {
-SETNAME1=$( cat $TEMPFOLDER/dangling_jellifish.feeder | cut -d '!' -f2 )
+SETNAME=$( cat $TEMPFOLDER/dangling_jellifish.feeder | cut -d '!' -f2 )
 if [ -x `which curl` -a -x `which ipset` ]; then
-   logger -t "feeder_block_stamparm_ip_block" "Adding IPs to be blocked."  
-   ipset flush $SETNAME1
+   logger -t "$( echo $SETNAME )" "Adding IPs to be blocked."  
+   ipset flush $SETNAME
    sleep 3
-   iptables -D INPUT 1 -m set --match-set $SETNAME1 src -j DROP
-   iptables -D FORWARD -m set --match-set $SETNAME1 src -j DROP
+   iptables -D INPUT 1 -m set --match-set $SETNAME src -j DROP
+   iptables -D FORWARD -m set --match-set $SETNAME src -j DROP
    sleep 3
-   ipset create $SETNAME1 iphash 2>/dev/null
+   ipset create $SETNAME
    sleep 2
-   iptables -I INPUT 1 -m set --match-set $SETNAME1 src -j DROP
-   iptables -A FORWARD -m set --match-set $SETNAME1 src -j DROP
-      for i in $feeder_block_stamparm_ips
-           do ipset add $SETNAME1 $i
+   iptables -I INPUT 1 -m set --match-set $SETNAME src -j DROP
+   iptables -A FORWARD -m set --match-set $SETNAME src -j DROP
+      for i in $SETNAME
+           do ipset add $SETNAME $i
       done
-   logger -t "feeder_block_stamparm_ip_block" "$( ipset list $SETNAME1 | wc -l )"
+   logger -t "$( echo $SETNAME )" "$( ipset list $SETNAME | wc -l )"
    echo -n "[$(date +"%d/%m/%Y %H:%M:%S")] " | tee -a $FILE
-   ipset list $SETNAME1 | head -7 | tee -a $FILE
+   ipset list $SETNAME | head -7 | tee -a $FILE
 fi
 }
 
